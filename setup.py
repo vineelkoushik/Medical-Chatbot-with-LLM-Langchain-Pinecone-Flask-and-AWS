58728922553c8dd6e82b31c210b3f453ee34c39a
Vineel Koushik <vineelkoushik2005@gmail.com>
2026-04-28 10:06:30 +0530

    updated project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+   name="Medical_ChatBot",
+   version="0.1.0",
+   author="Vineel Koushik",
+   author_email="vineelkoushik2005@gmail.com",
+   packages=find_packages(),
+   install_requires=[]
+) 
